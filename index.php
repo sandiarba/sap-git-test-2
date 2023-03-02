d63591e87f724fb0c96d0bdd4b3edd0896cacd4c
sandiarba <sandiarbaputra@gmail.com>
2023-03-02 09:57:35 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni excepturi suscipit accusantium amet ipsam praesentium animi non libero! Hic harum doloremque ab unde tempora similique architecto veritatis laboriosam nam nesciunt?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
